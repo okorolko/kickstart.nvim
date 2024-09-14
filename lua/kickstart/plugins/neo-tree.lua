6efca24cf2c95a1c7a439b151e4fa7f2b093001f
okorolko <oleg.korolko@beamery.com>
2024-09-14 10:47:01 +0200

    initial customization

--- lua/kickstart/plugins/neo-tree.lua
+++ lua/kickstart/plugins/neo-tree.lua
@@ -14,7 +14,35 @@ return {
     { '\\', ':Neotree reveal<CR>', desc = 'NeoTree reveal', silent = true },
   },
   opts = {
+    -- hijack_netrw_behavior = 'open_default',
+    buffers = {
+      follow_current_file = {
+        enabled = true, -- This will find and focus the file in the active buffer every time
+        --              -- the current file is changed while the tree is open.
+        leave_dirs_open = false, -- `false` closes auto expanded dirs, such as with `:Neotree reveal`
+      },
+    },
     filesystem = {
+      filtered_items = {
+        visible = true, -- This is what you want: If you set this to `true`, all "hide" just mean "dimmed out"
+        hide_dotfiles = false,
+        hide_gitignored = true,
+        hide_by_name = {
+          '.git',
+          -- '.DS_Store',
+          -- 'thumbs.db',
+        },
+        never_show = { -- remains hidden even if visible is toggled to true, this overrides always_show
+          '.git',
+          --".DS_Store",
+          --"thumbs.db"
+        },
+      },
+      follow_current_file = {
+        enabled = true, -- This will find and focus the file in the active buffer every time
+        --              -- the current file is changed while the tree is open.
+        leave_dirs_open = false, -- `false` closes auto expanded dirs, such as with `:Neotree reveal`
+      },
       window = {
         mappings = {
           ['\\'] = 'close_window',
